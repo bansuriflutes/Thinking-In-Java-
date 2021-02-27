535eea3a913b96053eb0557f136c045e62fd83bc
Oleksandr <fleitist@gmail.com>
2021-02-27 13:34:40 +0200

    created ShowProperties.java

--- src/main/java/com/makarov/practice/IntroductionToObjects/HelloDate.java
+++ src/main/java/com/makarov/practice/IntroductionToObjects/HelloDate.java
@@ -1,6 +1,11 @@
 package com.makarov.practice.IntroductionToObjects;
 
 import java.util.*;
+/**
+*@Class HelloDate.
+*@author fleitist@gmail.com
+*
+*/
 
 public class HelloDate {
   public static void main(String[] args) {
