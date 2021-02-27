84cb2b56b8edc67573c7d3c3332ea1f4371a490d
Oleksandr <fleitist@gmail.com>
2021-02-27 15:30:58 +0200

    Two tasks from exercises Chapter 2 are done

---
+++ src/main/java/com/makarov/practice/chapter2/exercises/Hello.java
@@ -0,0 +1,17 @@
+package com.makarov.practice.chapter2.exercises;
+
+import java.util.*;
+/**
+ * @class Hello.
+ * Prints massege "Hello, World!".
+ * @author Oleksandr Makarov
+ * @author fleitist@gmail.com
+ * @version 1.0
+ *
+*/
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
---
+++ src/main/java/com/makarov/practice/chapter2/exercises/Practice1.java
@@ -0,0 +1,27 @@
+package com.makarov.practice.chapter2.exercises;
+
+/**
+ * @class Practice1.
+ * Create clas Practise1 with 2 filds: int and char
+ * Prints their values
+ * @author Oleksandr Makarov.
+ * @author fleitist@gmail.com
+ * @version 1.0
+*/
+public class Practice1 {
+	
+  public static void main(String[] args) {
+		
+	/**
+	 *creating an uninitialized Integer field in the program.
+	*/
+	int a = 0;
+	/**
+	 *creating an uninitialized char field in the program.
+	*/
+	char b = ' ';
+	
+	System.out.println("Instance a = " + a);
+	System.out.println("Instance b = " + b);
+  }
+}
